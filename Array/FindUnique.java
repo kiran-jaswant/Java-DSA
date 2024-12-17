98d9f358e57798267b3461d48d6787af5aa349c3
kiran-jaswant <kiranjaswant08@gmail.com>
2024-12-18 00:17:51 +0530

    finding unique element from an array

---
+++ Array/FindUnique.java
@@ -0,0 +1,37 @@
+package Array;
+import  java.util.Scanner;
+public class FindUnique {
+    static int findUnique(int [] arr) {
+        int n = arr.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[i] = -1;
+                    arr[j] = -1;
+                }
+
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                 ans = arr[i];
+
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n= sc.nextInt();
+        System.out.println("Enter array elements:");
+        int [] arr= new int[n];
+        for (int i=0;i<n ;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        System.out.println("Unique element from given array is "+ findUnique(arr));
+
+    }
+}
